b06d5d3c2ba23d2ce8cd96a01315e9fb6bfdc02e
Anton Zhulanov <zhulanoff2007@gmail.com>
2021-01-19 23:35:49 +0300

    добавил фикстуру

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+import requests
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--url",
+        default="https://ya.ru",
+        help="This is request url"
+    )
+
+    parser.addoption(
+        "--status_code",
+        default="200",
+        help="expected status code",
+        type=int
+    )
+
+
+@pytest.fixture
+def base_url(request):
+    return request.config.getoption("--url")
+
+@pytest.fixture
+def expected_status(request):
+    return request.config.getoption("--status_code")
